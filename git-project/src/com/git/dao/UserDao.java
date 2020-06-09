647ffdcb363c25a0df3e45641b8ad85532889b41
HPY294328162 <29438162@qq.com>
2020-06-09 17:14:01 +0800

    push my project to github

---
+++ git-project/src/com/git/dao/UserDao.java
@@ -0,0 +1,20 @@
+package com.git.dao;
+
+import java.util.List;
+
+public class UserDao {
+	public void addUser(int id,String username, String password) {	
+	
+	}
+	public void deleteUser (int id) {
+		
+		
+	}
+	public void updateUser (int id, String newUsername,String newPassword) {
+		
+		
+	}
+	public List<Object>scanUser (int pageIndex,int pageSize){
+		return null;
+	}
+}
